d4ea8cdc81ce6c7f833d3cfab42fd57616ed135d
Rishav Aich <aich.1@iitj.ac.in>
2024-12-25 19:51:16 +0530

    created handler for vercel

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	// "log"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -9,15 +9,63 @@ import (
 	"github.com/gorilla/handlers"
 )
 
-func main() {
+// func main() {
 
+// 	r := mux.NewRouter()
+
+// 	fs := http.FileServer(http.Dir("static"))
+// 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+// 	r.HandleFunc("/", home_before_login)
+
+// 	// student
+// 	r.HandleFunc("/home/", home_after_login)
+// 	r.HandleFunc("/signup/", signup)
+// 	r.HandleFunc("/signin/", login)
+// 	r.HandleFunc("/club/", club_option)
+// 	r.HandleFunc("/inventory/{ID}/", inventory)
+// 	r.HandleFunc("/cart/", cart)
+// 	r.HandleFunc("/update/", update)
+// 	r.HandleFunc("/thank/", thank_page)
+// 	r.HandleFunc("/borrowlist/", borrow_list)
+// 	r.HandleFunc("/deleteItems/", delete_items)
+
+// 	// club 
+// 	r.HandleFunc("/clublogin/", coordinator_login)
+// 	r.HandleFunc("/clubhome/", club_home)
+// 	r.HandleFunc("/additem/", add_inventory)
+// 	r.HandleFunc("/updateinfo/", update_info)
+// 	r.HandleFunc("/clubborrowlist/", club_borrow_list)
+// 	r.HandleFunc("/inventorylist/", inventorylist)
+// 	r.HandleFunc("/inventorylist/edit/", edit_inventory)
+// 	r.HandleFunc("/inventorylist/delete/", delete_inventory)
+	
+
+// 	// Enable CORS for all routes
+// 	corsHandler := handlers.CORS(
+// 		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+// 		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"}),
+// 		handlers.AllowedOrigins([]string{"*"}),
+// 	)
+
+// 	// Apply CORS middleware to the router
+// 	http.Handle("/", corsHandler(r))
+
+// 	err := http.ListenAndServe(":8080", nil)
+// 	if err != nil {
+// 		log.Fatal("ListenAndServe: ", err)
+// 	}
+
+// }
+
+func Handler(w http.ResponseWriter, r *http.Request) {
 	r := mux.NewRouter()
 
+	// Serve static files
 	fs := http.FileServer(http.Dir("static"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
-	r.HandleFunc("/", home_before_login)
 
-	// student
+	// Define routes
+	r.HandleFunc("/", home_before_login)
 	r.HandleFunc("/home/", home_after_login)
 	r.HandleFunc("/signup/", signup)
 	r.HandleFunc("/signin/", login)
@@ -29,7 +77,7 @@ func main() {
 	r.HandleFunc("/borrowlist/", borrow_list)
 	r.HandleFunc("/deleteItems/", delete_items)
 
-	// club 
+	// Club routes
 	r.HandleFunc("/clublogin/", coordinator_login)
 	r.HandleFunc("/clubhome/", club_home)
 	r.HandleFunc("/additem/", add_inventory)
@@ -38,9 +86,8 @@ func main() {
 	r.HandleFunc("/inventorylist/", inventorylist)
 	r.HandleFunc("/inventorylist/edit/", edit_inventory)
 	r.HandleFunc("/inventorylist/delete/", delete_inventory)
-	
 
-	// Enable CORS for all routes
+	// Enable CORS
 	corsHandler := handlers.CORS(
 		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
 		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"}),
@@ -48,11 +95,5 @@ func main() {
 	)
 
 	// Apply CORS middleware to the router
-	http.Handle("/", corsHandler(r))
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-
+	corsHandler(r).ServeHTTP(w, r)
 }
