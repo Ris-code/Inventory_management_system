6f1fb97ee89987d191238fb8e5dc2afba786e7e2
Rishav Aich <aich.1@iitj.ac.in>
2024-12-25 20:00:34 +0530

    fixed the r variable issue

--- main.go
+++ main.go
@@ -58,35 +58,35 @@ import (
 
 // }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	r := mux.NewRouter()
+func Handler(w http.ResponseWriter, req *http.Request) {
+	router := mux.NewRouter()
 
 	// Serve static files
 	fs := http.FileServer(http.Dir("static"))
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
 
 	// Define routes
-	r.HandleFunc("/", home_before_login)
-	r.HandleFunc("/home/", home_after_login)
-	r.HandleFunc("/signup/", signup)
-	r.HandleFunc("/signin/", login)
-	r.HandleFunc("/club/", club_option)
-	r.HandleFunc("/inventory/{ID}/", inventory)
-	r.HandleFunc("/cart/", cart)
-	r.HandleFunc("/update/", update)
-	r.HandleFunc("/thank/", thank_page)
-	r.HandleFunc("/borrowlist/", borrow_list)
-	r.HandleFunc("/deleteItems/", delete_items)
+	router.HandleFunc("/", home_before_login)
+	router.HandleFunc("/home/", home_after_login)
+	router.HandleFunc("/signup/", signup)
+	router.HandleFunc("/signin/", login)
+	router.HandleFunc("/club/", club_option)
+	router.HandleFunc("/inventory/{ID}/", inventory)
+	router.HandleFunc("/cart/", cart)
+	router.HandleFunc("/update/", update)
+	router.HandleFunc("/thank/", thank_page)
+	router.HandleFunc("/borrowlist/", borrow_list)
+	router.HandleFunc("/deleteItems/", delete_items)
 
 	// Club routes
-	r.HandleFunc("/clublogin/", coordinator_login)
-	r.HandleFunc("/clubhome/", club_home)
-	r.HandleFunc("/additem/", add_inventory)
-	r.HandleFunc("/updateinfo/", update_info)
-	r.HandleFunc("/clubborrowlist/", club_borrow_list)
-	r.HandleFunc("/inventorylist/", inventorylist)
-	r.HandleFunc("/inventorylist/edit/", edit_inventory)
-	r.HandleFunc("/inventorylist/delete/", delete_inventory)
+	router.HandleFunc("/clublogin/", coordinator_login)
+	router.HandleFunc("/clubhome/", club_home)
+	router.HandleFunc("/additem/", add_inventory)
+	router.HandleFunc("/updateinfo/", update_info)
+	router.HandleFunc("/clubborrowlist/", club_borrow_list)
+	router.HandleFunc("/inventorylist/", inventorylist)
+	router.HandleFunc("/inventorylist/edit/", edit_inventory)
+	router.HandleFunc("/inventorylist/delete/", delete_inventory)
 
 	// Enable CORS
 	corsHandler := handlers.CORS(
@@ -96,5 +96,5 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	)
 
 	// Apply CORS middleware to the router
-	corsHandler(r).ServeHTTP(w, r)
+	corsHandler(router).ServeHTTP(w, req)
 }
